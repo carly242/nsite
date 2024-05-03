b9a16370e51d652ecee04ac10cc7f16343f0f21d
carly242 <carlybivihou8@gmail.com>
2024-05-03 11:21:48 +0200

    third commit

--- mon_projet/settings.py
+++ mon_projet/settings.py
@@ -112,13 +112,13 @@ DATABASES = {
     }
 }
 
-
+"""
 
 import dj_database_url
 DATABASES = {
     'default': dj_database_url.parse(env('DATABASE_URL'))
 }
-"""
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
